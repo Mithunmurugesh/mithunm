cc2bc78cc3f98310bb9ba2c0193f0742d373c19c
Mithunmurugesh <35652919+Mithunmurugesh@users.noreply.github.com>
2018-01-30 16:13:28 +0530

    Create large.c

---
+++ large.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+clrscr();
+printf("enter the values a,b,c");
+scanf("%d%d%d",&a,&b,&c);
+if(a>b&&a>c)
+{
+printf("a is the largest value");
+}
+else if(b>a&&b>c)
+{
+printf("b is the largest value");
+}
+else
+{
+printf("c is the largest value");
+}getch();
+}
