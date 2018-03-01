1b45b5ef6a7a79ca3ef5c001dd1e80fe0dab52d7
Mithunmurugesh <35652919+Mithunmurugesh@users.noreply.github.com>
2018-03-01 16:23:58 +0530

    Create mm27.c

---
+++ mm27.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+clrscr();
+printf("enter the value");
+scanf("%d",&a);
+if(a==0||a==1||a==2||a==3||a==4||a==5||a==6||a==7||a==8||a==9)
+{
+printf("yes");
+}
+else
+{
+printf("no");
+}
+getch();
+}
