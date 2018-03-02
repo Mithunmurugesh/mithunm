bbb0156289798f105cc94ad4d446c5a4b74708e3
Mithunmurugesh <35652919+Mithunmurugesh@users.noreply.github.com>
2018-03-02 15:10:09 +0530

    Create mm36.c

---
+++ mm36.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    char s[200];
+    int count=0,i,n;
+    clrscr();
+    scanf("%[^\n]s",s);
+    n=strlen(s);
+    for(i=0;s[i]!='\0';i++)
+    {
+        if(s[i]=='0'||s[i]=='1'||s[i]=='2'||s[i]=='3'||s[i]=='4'||s[i]=='5'||s[i]=='6'||s[i]=='7'||s[i]=='8'||s[i]=='9')
+            count++;    
+    }
+    printf("%d",count);
+   getch();
+}
