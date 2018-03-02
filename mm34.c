c57b822662ae06910f4f0bfb4204b95722b2796b
Mithunmurugesh <35652919+Mithunmurugesh@users.noreply.github.com>
2018-03-02 14:46:30 +0530

    Create mm34.c

---
+++ mm34.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char s[200];
+int count=0,i,n,ans;
+clrscr();
+scanf("%[^\n]s",s);
+n=strlen(s);
+for(i=0;s[i]!='\0';i++)
+{
+if(s[i]=='.')
+count++;
+}
+printf("%d",count+1);
+getch();
+}
