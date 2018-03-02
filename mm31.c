fd23e856df981a3a6786624c3fe66ec073ab77a0
Mithunmurugesh <35652919+Mithunmurugesh@users.noreply.github.com>
2018-03-02 14:14:51 +0530

    Create mm31.c

---
+++ mm31.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char a[10];
+int i,n;
+clrscr();
+printf("enter the letters");
+scanf("%d",&a[i]);
+if(a[i]>='a'&&a[i]<='z'||a[i]>='A'&&a[i]<='Z')
+{
+n++;
+}
+i++;
+getch();
+}
