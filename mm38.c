ba500665a9c6b8cd5ae83104e46c793532e3ab63
Mithunmurugesh <35652919+Mithunmurugesh@users.noreply.github.com>
2018-03-03 16:53:29 +0530

    Create mm38.c

---
+++ mm38.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int i=3,j=5;
+    clrscr();
+    printf("i=%d,j=%d",i,j);
+    i=i^j;
+    j=i^j;
+    i=i^j;
+    printf("i=%d,j=%d",i,j);
+    getch();
+}
