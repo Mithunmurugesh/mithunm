c65dbe64a6afc917a07fad24da11b65511a1c7c7
Mithunmurugesh <35652919+Mithunmurugesh@users.noreply.github.com>
2018-03-13 16:18:01 +0530

    Create mm43.c

---
+++ mm43.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include<string.h>
+#include<conio.h>
+void main() 
+{
+	char a[10],b[10];
+	int n,n1,i,j;
+  clrscr();
+	scanf("%s",a);
+	scanf("%s",b);
+	n=strlen(a);
+	n1=strlen(b);
+	for(i=0;i<n1;i++)
+	{
+	    a[n]=b[i];
+	    n++;
+	}
+    	printf("%s",a);
+   	getch();
+}
