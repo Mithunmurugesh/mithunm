56658b24a82d9a2c60f16a6a6ef7ab983acdc800
Mithunmurugesh <35652919+Mithunmurugesh@users.noreply.github.com>
2018-03-13 16:23:23 +0530

    Create mm44.c

---
+++ mm44.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main() 
+{
+	int n;
+  clrdcr();
+	scanf("%d",&n);
+    if(n>=1 && n<=10)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+	getch();
+}
