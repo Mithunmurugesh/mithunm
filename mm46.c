b0a8675d793829f27644a053bcf02c9e7c7d0c72
Mithunmurugesh <35652919+Mithunmurugesh@users.noreply.github.com>
2018-03-13 16:46:03 +0530

    Create mm46.c

---
+++ mm46.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include<conio.h>
+void main()
+{
+    int a,b;
+    clrscr();
+    printf("enter a value");
+    scanf("%d",&a);
+    b=++a;
+    printf("%d",b);
+    getch();
+}
