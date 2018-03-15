9cd514e930a41b40e13ae8ed25c215866619e6d9
Mithunmurugesh <35652919+Mithunmurugesh@users.noreply.github.com>
2018-03-15 15:52:42 +0530

    Create mm49.c

---
+++ mm49.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,sum=0;
+clrscr();
+printf("enter a value");
+printf("enter b value");
+scanf("%d",&a);
+scanf("%d",&b);
+sum=a+b;
+printf("%d",sum);
+getch();
+}
