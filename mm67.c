1d3adb28e437a445779e2f4a80bc05c6cf335e75
Mithunmurugesh <35652919+Mithunmurugesh@users.noreply.github.com>
2018-03-20 16:57:24 +0530

    Create mm67.c

---
+++ mm67.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int n,num;
+    clrscr();
+    scanf("%d",&num);
+    n=num+1;
+    while(n!=0)
+    {
+        if(n%10==0)
+        {
+            printf("%d",n);
+            break;
+        }
+        n++;
+    }
+    getch();
+}
