b670111135655a079fc4f39a7f3afcc91a3c3926
Mithunmurugesh <35652919+Mithunmurugesh@users.noreply.github.com>
2018-03-20 16:45:25 +0530

    Create mm65.c

---
+++ mm65.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    char a[100];
+    int i;
+    clrscr();
+    scanf("%[^\t\n]s",&a);
+    if(printf("%s",a))
+    {
+        
+    }
+    getch();
+}
